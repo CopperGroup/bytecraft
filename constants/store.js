e8287f4549d580cb152332b8d2a112027cfb488d
tymyevhenmpw <tymyevhen@gmail.com>
2025-04-03 22:18:43 +0200

    first commit

---
+++ constants/store.js
@@ -0,0 +1,12 @@
+export const Store = {
+  "name": "ByteCraft",
+  "currency": "UAH",
+  "currency_sign": "₴",
+  "domain": "https://fo-scandinavia.vercel.app",
+  "database": "ByteCraftPeripherals_2025_03_16",
+  "default_image": {
+    "product_card": 0,
+    "product_page": 0,
+    "checkout_page": 0
+  }
+};
