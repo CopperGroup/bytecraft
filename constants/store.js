66dd4d114eda8039f2844f1b0a86c9e514348dc0
tymyevhenmpw <tymyevhen@gmail.com>
2025-05-03 00:06:25 +0200

    commit

--- constants/store.js
+++ constants/store.js
@@ -2,7 +2,7 @@ export const Store = {
   "name": "ByteCraft",
   "currency": "UAH",
   "currency_sign": "₴",
-  "domain": "https://bytecraftua.com ",
+  "domain": "https://wwwbytecraftua.com ",
   "database": "ByteCraftPeripherals_2025_03_16",
   "default_image": {
     "product_card": 0,
